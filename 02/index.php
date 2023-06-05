4e298ece921dfa613f3e612bd6efd9080a93c77a
Aleksey Goryn <fin.07@bk.ru>
2023-06-05 11:21:19 +0300

    Уроки 2 и 3

---
+++ 02/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <title>Урок 2</title>
+    </head>
+
+    <body>
+
+        <p>Привет мир!</p>
+
+        <?php print_r('Привет мир2!'); ?>
+        
+        <?php echo '<p>Привет мир3!</p>'; ?>
+
+    </body>
+</html>
---
+++ 03/index.php
@@ -0,0 +1,21 @@
+<?php /* phpinfo(); */ ?>
+<?php // phpinfo(); ?>
+<?php # phpinfo(); В стиле Unix ?>
+
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <title>Урок 3</title>
+    </head>
+
+    <body>
+
+        <p>Привет мир!</p>
+
+        <?php echo'<p>Привет мир2!</p>'; ?>
+
+        <?= '<p>Привет мир3!</p>'; ?>
+
+    </body>
+</html>
